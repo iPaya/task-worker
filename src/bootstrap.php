34f753ecc1c2dd75fd2d2f10d7542b7b59d18905
Zhang Di <zhangdi_me@163.com>
2018-06-28 13:43:48 +0800

    Fixed using bin/worker in vendor/bin/worker

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,11 +1,21 @@
 <?php
 
-define('ROOT_PATH',dirname(__DIR__));
+define('TASK_WORKER_ROOT_PATH', dirname(__DIR__));
 
-require_once(ROOT_PATH . '/vendor/autoload.php');
+$composerAutoload = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../autoload.php',
+];
+$vendorPath = null;
+foreach ($composerAutoload as $autoload) {
+    if (file_exists($autoload)) {
+        require $autoload;
+        $vendorPath = dirname($autoload);
+        break;
+    }
+}
 
-$envFile = ROOT_PATH . '/.env';
-
+$envFile = TASK_WORKER_ROOT_PATH . '/.env';
 if (file_exists($envFile)) {
     // 从 env.ini 加载环境变量
     $dotenv = new \Symfony\Component\Dotenv\Dotenv();
